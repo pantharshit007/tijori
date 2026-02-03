61af4db6182b6cf319cd1c38ce094a843cae379a
pantharshit007 <aaryanpandit3527@gmail.com>
2026-02-03 12:26:21 +0530

    feat: deployment setup

--- convex/lib/errors.ts
+++ convex/lib/errors.ts
@@ -1,5 +1,5 @@
 import { ConvexError } from "convex/values";
-import type { Id } from "convex/_generated/dataModel";
+import type { Id } from "../_generated/dataModel";
 
 /**
  * Standard error types for the application.
